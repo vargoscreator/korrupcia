3d4bbf94587b8dfbfa18cf33611939f8458c3d58
vargoscreator <bacahoriyk@gmail.com>
2025-04-13 12:50:55 +0300

    add pages

---
+++ js/merch.js
@@ -0,0 +1,9 @@
+let reviewsSwiper = new Swiper(".merch__slider", {
+    loop: false,
+    spaceBetween: 0,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".merch__slider-next",
+        prevEl: ".merch__slider-prev",
+    },
+});
