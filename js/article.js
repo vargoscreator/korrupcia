e053e7de1a1f7be9218144ac31b7d1f3ec26f9e6
vargoscreator <bacahoriyk@gmail.com>
2025-04-15 13:22:06 +0300

    add slider

---
+++ js/article.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let heroSwiper = new Swiper(".article__image", {
+        loop: false,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: ".article__image-next",
+            prevEl: ".article__image-prev",
+        },
+    });
+});
